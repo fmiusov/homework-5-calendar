3e4dc2dcb5d00eeb5ecc337b9fbd6ceb13d9c4e0
JaysonD <jaduff20@gmail.com>
2020-03-13 18:08:55 -0400

    local storage attempts, explanation given

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
+  
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-  // WHEN I view the timeblocks for that day
-  // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+  //I hardcoded the HTML and then spent the majority of my time trying to get the Class addition
+  //conditional to work correctly. By the time I realized I had to generate the rows dynamically, it 
+  //was already Friday. I tried to restart the project, but could not get the rows to generate in the alotted
+  //time, so I am submitting this attempt version as it has more substance
 
+  //The conditional works (as proven in the console) but it will not remove Classes, even with the .removeClass method
+  //This issue arose because I hardcoded the HTML, misunderstanding the aim of the assignment
   for (var i = 0; i < 9; i++) {
     hourRow = $(".hour").children().prevObject[i];
     currentHour = moment().format("k");
     hourChange = $("textarea");
-    console.log(hourRow.id)
     if (parseInt(currentHour) > parseInt(hourRow.id)) {
       hourChange.addClass("past");
     } else if (parseInt(currentHour) === parseInt(hourRow.id)) {
@@ -18,13 +22,19 @@ $(document).ready(function() {
     }
   }
 
-  $("textarea").on
 
-  // WHEN I click the save button for that timeblock
 
-  // THEN the text for that event is saved in local storage
+  //Similar difficulties with local storage when dealing with hardcoded HTML
+  //My attempt here was to set the textarea to an input field and grab that input value when the save button was clicked
+  //Then the value would be stored as an object in local storage
+  $(".saveBtn").on("click", function(){
+    $("textarea").append($("<form>").append(("<input>")))
+    $("input").attr("type", "text")
+    var toDoItem = $("input").value.trim()
+    var toDoObj = {
+      todo: toDoItem
+    }
+    localStorage.setItem("To Do", toDoObj);
+  })
 
-  // WHEN I refresh the page
-
-  // THEN the saved events persist
 });
