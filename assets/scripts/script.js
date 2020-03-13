6b034fb1ace85f759bd700592614f43742ed0fe6
JaysonD <jaduff20@gmail.com>
2020-03-13 16:11:12 -0400

    STARTING OVER FROM SCRATCH because HTML was not generated dynamically and it was breaking everything. RIP

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,17 +5,28 @@ $(document).ready(function() {
   // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
   for (var i = 0; i < 9; i++) {
-    hourRow = $(".hour").children().prevObject[i];
-    currentHour = moment().format("k");
-    hourChange = $("textarea");
+    var hourRow = $(".hour").children().prevObject[i];
+    var currentHour = moment().format("k");
+    var hourChange = ($("textarea")[i]);
     console.log(hourRow.id)
+    console.log(currentHour)
+    console.log(hourChange)
     if (parseInt(currentHour) > parseInt(hourRow.id)) {
+      hourChange.removeClass("future present")
       hourChange.addClass("past");
+      
+      console.log("past")
     } else if (parseInt(currentHour) === parseInt(hourRow.id)) {
+      hourChange.removeClass("future past")
       hourChange.addClass("present");
+      
+      console.log("present")
     } else if (parseInt(currentHour) < parseInt(hourRow.id)) {
+      hourChange.removeClass("present past")
       hourChange.addClass("future");
+      console.log("future")
     }
+    
   }
 
   // WHEN I click into a timeblock
